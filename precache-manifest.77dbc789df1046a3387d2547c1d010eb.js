a27bb490fe6b2005957f54237839ab93b39faa98
DiGregorioC <digregorioc@outlook.com>
2019-08-05 21:02:46 -0400

    Updates

---
+++ precache-manifest.77dbc789df1046a3387d2547c1d010eb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08b255e7b98325ed573a0b289027f765",
+    "url": "/cd-youtube-clone/index.html"
+  },
+  {
+    "revision": "29c8afe6b6f54d2ac899",
+    "url": "/cd-youtube-clone/static/js/2.52fb1e22.chunk.js"
+  },
+  {
+    "revision": "e3e77f6aa01cd248f7bd",
+    "url": "/cd-youtube-clone/static/js/main.1936f3f6.chunk.js"
+  },
+  {
+    "revision": "fbf167fc70ee79b8a0f7",
+    "url": "/cd-youtube-clone/static/js/runtime~main.a8ba0ab2.js"
+  }
+]);
